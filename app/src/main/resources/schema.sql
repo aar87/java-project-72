96454e4ddc9e66312ba60bf364fa14dc43840cba
AntonRaevsky <aanraevskiy@sberbank.ru>
2024-11-16 16:58:45 +0300

    fix sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,15 +1,15 @@
  CREATE TABLE IF NOT EXISTS urls (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS url_checks (
-    id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY NOT NULL,
     url_id BIGINT REFERENCES urls(id) NOT NULL,
     status_code INT,
-    title VARCHAR(1024),
-    h1 VARCHAR(1024),
+    title VARCHAR(255),
+    h1 VARCHAR(255),
     description TEXT,
-    created_at TIMESTAMP
+    created_at TIMESTAMP NOT NULL
 );
